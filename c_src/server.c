b4e43f3dbf9293988dcc25f22c5d841dd2c81015
Jongyeon Yoon <0417yjy@naver.com>
2019-11-30 13:32:55 -0800

    Add subscription functions (doesn't work yet)

--- c_src/server.c
+++ c_src/server.c
@@ -24,6 +24,7 @@ typedef struct client_node {
 	char password[31];
 	char sublist_fname[43];
 	char twittlist_fname[45];
+	int sublist_arr[USERS - 1];
 	client_link next;
 } client_node;
 
@@ -55,30 +56,232 @@ void delete_node(client_link head, client_link deleted_node) {
 	free(deleted_node);
 }
 
-int makeFileofString(char *filename, char *str) {
-	FILE *f;
-	f = fopen(filename, "w");
-	if (f == NULL) {
-		printf("fopen failed!\n");
-		return 1;
-	}
-
-	fputs(str, f);
-	fclose(f);
-	return 0;
+void send_message(int sockfd, char *message) {
+	char buffer[MSG_LEN];
+	printf("Sent \n%s to %d..\n", message, sockfd);
+	bzero(buffer, MSG_LEN);
+	strcpy(buffer, message);
+	write(sockfd, buffer, strlen(buffer));
 }
 
-int readFiletoString(char *filename, char *str) {
+void read_subscriptions(int user_idx, client_link np) {
 	FILE *f;
-	f = fopen(filename, "r");
-	if (f == NULL) {
-		printf("fopen failed!\n");
-		return 1;
-	}
+	char filename[43];
+	char tmp_buffer[5];
+	bool make_new = false;
+	int subscribe_idx = 0;
 
-	fscanf(f, "%s", str);
+	sprintf(filename, "%d_subs.txt", user_idx);
+	f = fopen(filename, "r");
+	while (f == NULL) {
+		printf("fopen failed!\n");
+		f = fopen(filename, "w"); // if file doesn't exist, make a new one
+		make_new = true;
+	}
+	
+	if(!make_new) {
+		while(fgets(tmp_buffer, 4, f) != NULL) { // read subscribe index
+			tmp_buffer[strlen(tmp_buffer) - 1] = '\0'; // change '\n' in the last column to '\0'
+			np->sublist_arr[subscribe_idx++] = atoi(tmp_buffer); // store into subscribe list array
+		}
+		for(;subscribe_idx < USERS - 1;subscribe_idx++) {
+			np->sublist_arr[subscribe_idx] = -1; // store -1 into rest indices as a blank
+		}
+	}
+	else {
+		for(subscribe_idx = 0;subscribe_idx < USERS - 1;subscribe_idx++) {
+			fprintf(f, "-1\n");
+			np->sublist_arr[subscribe_idx] = -1; // store -1 into rest indices as a blank
+		}
+	}
 	fclose(f);
-	return 0;
+}
+
+void show_subscriptions(int user_idx, client_link np) {
+	int i;
+	char buffer[5];
+
+	send_message(np->sockfd, "Your subscription list:\n");
+	if(np->sublist_arr[0] == -1) {
+		send_message(np->sockfd, "You subscribed no one! Add someone please.\n");
+		return;
+	}
+	for(i=0;i<USERS-1;i++) {
+		if(np->sublist_arr[i] == -1) {
+			break;
+		}
+		bzero(buffer, 5);
+		sprintf(buffer, "%s\n", user_arr[np->sublist_arr[i]]);
+		write(np->sockfd, buffer, strlen(buffer));
+	}
+}
+
+void add_subscription(int user_idx, client_link np) {
+	char buffer[31];
+	char recv_buffer[MSG_LEN];
+	char filename[43];
+	int i, j, rcv;
+	FILE *f;
+	
+	// get user input
+	send_message(np->sockfd, "Insert name of whom you want to subscribe: ");
+	bzero(buffer, 31);
+	if(rcv = read(np->sockfd, buffer, 31) < 0) {
+		printf("Client(%s) didn't reply.\n", np->ip);
+		return;
+	}
+	while(strlen(recv_buffer) == 0) { // doesn't work
+		rcv = read(np->sockfd, buffer, 31);
+	}
+	buffer[strlen(buffer) - 1] = '\0';
+
+	// search inserted name
+	for(i=0;i<USERS;i++) {
+		if(user_idx != i) { // search except client himself
+			if(strcmp(buffer, user_arr[i]) == 0) { // detected
+				// send confirmation message
+				bzero(recv_buffer, MSG_LEN);
+				sprintf(recv_buffer, "%s has been found: ID is %d. Is it corret?\n", buffer, i);
+				write(np->sockfd, recv_buffer, strlen(recv_buffer));
+
+				// get client's reply
+				bzero(recv_buffer, MSG_LEN);
+				if(rcv = read(np->sockfd, recv_buffer, MSG_LEN) < 0) {
+					printf("Client(%s) didn't reply.\n", np->ip);
+					return;
+				}
+				else if(rcv >=0) {
+					while(strlen(recv_buffer) == 0) {
+						rcv = read(np->sockfd, recv_buffer, 31);
+					}
+					//printf("got reply y/n %s sent %s\n", np->ip, recv_buffer);
+					while((strcmp(recv_buffer, "n") != 0) && (strcmp(recv_buffer, "y") != 0)) {
+						// user didn't reply with (y/n)
+						send_message(np->sockfd, "Please reply with 'y' or 'n'.\n");
+						bzero(recv_buffer, MSG_LEN);
+						if(read(np->sockfd, recv_buffer, MSG_LEN) <= 0) {
+							printf("Client(%s) didn't reply.\n", np->ip);
+							break;
+						}
+						//printf("got reply y/n %s sent %s\n", np->ip, recv_buffer);
+					}
+					if(strcmp(recv_buffer, "n") == 0) {
+						// user replied with 'n'
+						send_message(np->sockfd, "Search another person..\n");
+					}
+					else {
+						// user replied with 'y'
+						for(j=0;j<USERS - 1;j++) {
+							if(np->sublist_arr[j] == i) {
+								// already subscribed
+								send_message(np->sockfd, "Name exists in your subscription list!\n");
+							}
+							if(np->sublist_arr[j] == -1) { // add subscription into blank
+								np->sublist_arr[j] = i;
+
+								// append to the sublist file
+								sprintf(filename, "%d_subs.txt", user_idx);
+								f = fopen(filename, "a");
+								fprintf(f, "%d\n", i);
+								fclose(f);
+								send_message(np->sockfd, "Subscribed");
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	// Couldn't find the name
+	send_message(np->sockfd, "Couldn't find that name!");
+}
+
+void delete_subscription(int user_idx, client_link np) {
+	char buffer[31];
+	char recv_buffer[MSG_LEN];
+	char filename[43];
+	int i, j, rcv;
+	FILE *f;
+	
+	// get user input
+	send_message(np->sockfd, "Insert name of whom you want to delete subscription: ");
+	bzero(buffer, 31);
+	if(rcv = read(np->sockfd, buffer, MSG_LEN) < 0) {
+		printf("Client(%s) didn't reply.\n", np->ip);
+		return;
+	}
+	while(strlen(recv_buffer) == 0) {
+		rcv = read(np->sockfd, buffer, MSG_LEN);
+	}
+	buffer[strlen(buffer) - 1] = '\0';
+
+	// search inserted name
+	for(i=0;i<USERS - 1;i++) {
+		if(np->sublist_arr[i] == -1) {
+			// couldn't find the name
+			break;
+		}
+		if(strcmp(buffer, user_arr[np->sublist_arr[i]]) == 0) {
+			// send confirmation message
+			bzero(recv_buffer, MSG_LEN);
+			sprintf(recv_buffer, "%s has been found: ID is %d. Is it corret?\n", buffer, np->sublist_arr[i]);
+			write(np->sockfd, recv_buffer, strlen(recv_buffer));
+
+			// get client's reply
+			bzero(recv_buffer, MSG_LEN);
+			if(rcv = read(np->sockfd, recv_buffer, MSG_LEN) < 0) {
+				printf("Client(%s) didn't reply.\n", np->ip);
+				return;
+			}
+			else if(rcv >=0) {
+				while(strlen(recv_buffer) == 0) {
+					rcv = read(np->sockfd, recv_buffer, 31);
+				}
+				//printf("got reply y/n %s sent %s\n", np->ip, recv_buffer);
+				while((strcmp(recv_buffer, "n") != 0) && (strcmp(recv_buffer, "y") != 0)) {
+					// user didn't reply with (y/n)
+					send_message(np->sockfd, "Please reply with 'y' or 'n'.\n");
+					bzero(recv_buffer, MSG_LEN);
+					if(read(np->sockfd, recv_buffer, MSG_LEN) <= 0) {
+						printf("Client(%s) didn't reply.\n", np->ip);
+						return;
+					}
+					//printf("got reply y/n %s sent %s\n", np->ip, recv_buffer);
+				}
+				if(strcmp(recv_buffer, "n") == 0) {
+					// user replied with 'n'
+					send_message(np->sockfd, "Search another person..\n");
+				}
+				else {
+					// user replied with 'y'
+					for(j=i;j<USERS-2;j++) {
+						// shift array left once
+						np->sublist_arr[j] = np->sublist_arr[j+1];
+					}
+					// make last subscription blank
+					np->sublist_arr[USERS-2] = -1;
+	
+					// edit sublist file
+					sprintf(filename, "%d_subs.txt", user_idx);
+					f = fopen(filename, "w");
+					for(j=0;j<USERS - 1;j++) {
+						if(np->sublist_arr[j] == -1) {
+							break;
+						}
+						fprintf(f, "%d\n", np->sublist_arr[j]);
+					}
+
+					// close file and return
+					fclose(f);
+					send_message(np->sockfd, "Subscribed");
+					return;
+				}
+			}
+		}
+	}
+	// Couldn't find the name
+	send_message(np->sockfd, "Couldn't find that name!");
 }
 
 int authenticate(char *usrname, char*pw) {
@@ -96,14 +299,6 @@ int authenticate(char *usrname, char*pw) {
 	return -1;
 }
 
-void send_message(int sockfd, char *message) {
-	char buffer[MSG_LEN];
-	printf("Sent \n%s to %d..\n", message, sockfd);
-	bzero(buffer, MSG_LEN);
-	strcpy(buffer, message);
-	write(sockfd, buffer, strlen(buffer));
-}
-
 enum menus {main_menu, write_menu, read_menu, subscribe_menu};
 void send_menu(int sockfd, int current_menu) {
 	char buffer[MSG_LEN];
@@ -111,7 +306,7 @@ void send_menu(int sockfd, int current_menu) {
 	bzero(buffer, MSG_LEN);	
 	switch(current_menu) {
 		case main_menu:
-		send_message(sockfd, "***** Main Menu *****\n1: Write a tweet\n2: Read tweets\n3: Set subscriptions\n4: Exit\n*********************\n");
+		send_message(sockfd, "***** Main Menu *****\n1: Write a tweet\n2: Read tweets\n3: Set subscriptions\n4: Logout\n*********************\n");
 		break;
 		
 		case write_menu:
@@ -177,6 +372,7 @@ void client_thread(void *client_node_addr) {
 					// insert user info
 					strcpy(np->username, username);
 					strcpy(np->password, password);
+					read_subscriptions(user_idx, np);
 
 					// send welcome message
 					send_message(np->sockfd, "Authenticated");
@@ -299,15 +495,15 @@ void client_thread(void *client_node_addr) {
 				case subscribe_menu:
 				switch(menu_selected) {
 					case 1:
-					//show_subscriptions();
+					show_subscriptions(user_idx, np);
 					break;
 
 					case 2:
-					//add_subscription();
+					add_subscription(user_idx, np);
 					break;
 
 					case 3:
-					//delete_subscription();
+					delete_subscription(user_idx, np);
 					break;
 
 					case -1:
@@ -396,7 +592,6 @@ int main(void)
 	}
 
   }
- 
  
   return 0;
 }
